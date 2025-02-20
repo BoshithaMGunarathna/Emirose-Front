c1a235d6c1632faaca3578e64ece85c71af563ce
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-02-20 17:00:24 +0530

    Create index.tsx

---
+++ public/icons/index.tsx
@@ -0,0 +1,17 @@
+// icons/LongArrow.tsx or icons/index.ts
+export const longArrow = () => (
+  <svg width="64" height="24" viewBox="0 0 64 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M52.5 18C52.5 18 58.5 13.5811 58.5 12C58.5 10.4188 52.5 6 52.5 6" stroke="#491247" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M45.5 18C45.5 18 51.5 13.5811 51.5 12C51.5 10.4188 45.5 6 45.5 6" stroke="#491247" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<line y1="11.5" x2="39" y2="11.5" stroke="#491247"/>
+</svg>
+
+);
+
+
+export const heart = () => (
+<svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M28.3823 5.8248C24.4715 3.42596 21.0583 4.39266 19.0078 5.93252C18.167 6.5639 17.7467 6.87959 17.4993 6.87959C17.252 6.87959 16.8317 6.5639 15.9908 5.93252C13.9405 4.39266 10.5272 3.42596 6.61645 5.8248C1.48403 8.97304 0.32269 19.3592 12.1612 28.1216C14.416 29.7905 15.5434 30.625 17.4993 30.625C19.4553 30.625 20.5827 29.7905 22.8376 28.1216C34.676 19.3592 33.5146 8.97304 28.3823 5.8248Z" stroke="#491247" stroke-width="1.5" stroke-linecap="round"/>
+</svg>
+
+);
