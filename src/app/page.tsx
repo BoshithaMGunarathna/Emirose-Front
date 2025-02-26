513937e5214e0d8378782614af46d3740adefbb0
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-02-26 13:16:54 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,18 @@ import { HeroSection } from "@/components/hero-section"
 import { FeaturedProducts } from "@/components/featured-products"
 import EventDecorSection from "@/components/EventDecorSection"
 import {GallerySection} from "@/components/gallery-section"
+import { SiteFooter } from "@/components/footer"
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen min-w-full">
       <SiteHeader />
-      <main className="flex-grow">
+      <main className="flex-grow  ">
         <HeroSection  />
         <FeaturedProducts  />
         <EventDecorSection  />
         <GallerySection  />
+        <SiteFooter  />
       </main>
     </div>
   )
